38a793e70d5a3f7e85ad8a5af0e7bcb43fae6b5b
minhtuan2001 <minhtuandev2001@gmail.com>
2024-06-20 12:10:20 +0700

    finish

--- src/constans/url.js
+++ src/constans/url.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://task-manager-backend-u8y2.onrender.com"
-export const BASE_URL = "http://localhost:4000"
+export const BASE_URL = "https://task-manager-backend-u8y2.onrender.com"
+// export const BASE_URL = "http://localhost:4000"
